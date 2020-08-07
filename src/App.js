2861af85da2278477c48fccc305512bb67060512
gchartier <gabriel@chartier.link>
2020-08-06 22:50:10 -0400

    Created menu and about me section:
    Added expandable modal menu in the header using react-burger-menu.
    
    Added about section to be rendered as a modal and wrote the copy.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import "./components/Header";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
-import About from "./components/About";
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <About />
             <MainContent />
             <Footer />
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,109 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
+import Menu from "react-burger-menu/lib/menus/slide";
 
 const StyledHeader = styled.header`
     text-align: left;
-    padding-left: 2%;
 `;
 
 const H1 = styled.h1`
     font-size: 2rem;
+    padding-left: 2%;
 `;
 
 const PrimarySpan = styled.span`
     color: #ffce80;
 `;
 
-function Header() {
-    return (
-        <StyledHeader>
-            <H1>
-                Indie<PrimarySpan>Tube</PrimarySpan>
-            </H1>
-        </StyledHeader>
-    );
+var menuStyles = {
+    bmBurgerButton: {
+        position: "absolute",
+        width: "36px",
+        height: "30px",
+        right: "36px",
+        top: "30px",
+    },
+    bmBurgerBars: {
+        background: "#373a47",
+    },
+    bmCrossButton: {
+        height: "24px",
+        width: "24px",
+    },
+    bmCross: {
+        background: "#bdc3c7",
+    },
+    bmMenuWrap: {
+        position: "fixed",
+        height: "100%",
+    },
+    bmMenu: {
+        background: "#373a47",
+        padding: "2.5em 1.5em 0",
+        fontSize: "1.15em",
+    },
+    bmMorphShape: {
+        fill: "#373a47",
+    },
+    bmItemList: {
+        color: "#b8b7ad",
+        padding: "0.8em",
+    },
+    bmItem: {
+        display: "inline-block",
+        color: "#ffce80",
+        fontSize: "1.5rem",
+        cursor: "pointer",
+    },
+    bmOverlay: {
+        height: "100vh",
+        background: "rgba(0, 0, 0, 0.3)",
+    },
+};
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuOpen: false,
+        };
+    }
+
+    handleStateChange(state) {
+        this.setState({ menuOpen: state.isOpen });
+    }
+
+    closeMenu() {
+        this.setState({ menuOpen: false });
+    }
+
+    openAbout() {
+        this.setState((state) => ({ menuOpen: !state.menuOpen }));
+    }
+
+    render() {
+        return (
+            <StyledHeader>
+                <H1>
+                    indie<PrimarySpan>Tube</PrimarySpan>
+                </H1>
+                <Menu
+                    right
+                    styles={menuStyles}
+                    isOpen={this.state.menuOpen}
+                    onStateChange={(state) => this.handleStateChange(state)}
+                >
+                    <a
+                        id="About"
+                        className="menu-item"
+                        onClick={() => this.closeMenu()}
+                    >
+                        About
+                    </a>
+                </Menu>
+            </StyledHeader>
+        );
+    }
 }
 
 export default Header;
