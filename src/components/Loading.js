ac1ebd71e23a9e2fb9bb133539920dc04e9e5d5b
gchartier <gabriel@chartier.link>
2020-08-09 19:29:52 -0400

    Installed react-loading and added loading comp.

---
+++ src/components/Loading.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactLoading from "react-loading";
+import styled from "styled-components";
+
+const LoadingContainer = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+function Loading() {
+    return ReactDOM.createPortal(
+        <LoadingContainer>
+            <ReactLoading
+                type={"bubbles"}
+                color={"#ffce80"}
+                height={"35%"}
+                width={"35%"}
+            />
+        </LoadingContainer>,
+        document.querySelector("#modal")
+    );
+}
+
+export default Loading;
