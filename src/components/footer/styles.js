a65b6f0ea74e631a16d1dc819127b9accd0107ad
Gabriel Chartier <gabriel@chartier.link>
2021-05-06 23:35:13 -0400

    Made footer sticky. Made size of loading animation smaller.

--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const StyledFooter = styled.footer`
     text-align: center;
@@ -6,20 +6,22 @@ export const StyledFooter = styled.footer`
     background-color: ${(props) => props.theme.bgColorPrimary};
     padding: 2%;
     box-shadow: 0px -5px 10px -10px rgba(0, 0, 0, 0.5);
-`;
+    position: sticky;
+    bottom: 0;
+`
 
 export const P = styled.p`
     font-weight: bold;
     font-size: calc(15px + (18 - 15) * ((100vw - 300px) / (1600 - 300)));
     margin: 0;
-`;
+`
 
 export const FlexContainer = styled.span`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-`;
+`
 
 export const SiteLink = styled.a`
     color: ${(props) => props.color || "black"};
-`;
+`
--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,22 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ReactLoading from "react-loading";
-import * as ui from "./styles";
-import { useContext } from "react";
-import { ThemeContext } from "styled-components";
+import React from "react"
+import ReactDOM from "react-dom"
+import ReactLoading from "react-loading"
+import * as ui from "./styles"
+import { useContext } from "react"
+import { ThemeContext } from "styled-components"
 
 export default function Loading() {
-    const themeContext = useContext(ThemeContext);
+    const themeContext = useContext(ThemeContext)
 
     return ReactDOM.createPortal(
         <ui.LoadingContainer>
             <ReactLoading
                 type={"bubbles"}
                 color={themeContext.primaryYellow}
-                height={"25%"}
-                width={"25%"}
+                height={"4rem"}
+                width={"4rem"}
             />
         </ui.LoadingContainer>,
         document.querySelector("#modal")
-    );
+    )
 }
